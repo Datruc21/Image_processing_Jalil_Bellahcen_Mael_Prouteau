b3b417c0307f89f566f675ac02b888449c043a80
Maël <78041675+Astrion20@users.noreply.github.com>
2025-03-21 12:03:23 +0100

    Hi

---
+++ bmp8.c
@@ -0,0 +1,37 @@
+#include "bmp8.h"
+#include <stdlib.h>
+
+#include <stdio.h>
+
+t_bmp8 * bmp8_loadImage(const char * filename){
+    t_bmp8 * new_image = (t_bmp8 *)malloc(sizeof(t_bmp8));
+    if (new_image == NULL ) return NULL;
+    unsigned char *header = (unsigned char*)malloc(sizeof(unsigned char)*54);
+    if (header == NULL)
+        printf("Error with memory location");
+
+    unsigned char* colorTable = (unsigned char*)malloc(sizeof(unsigned char)*1024);
+    unsigned char* data = (unsigned char*)malloc(sizeof(unsigned char));
+
+    unsigned int width;
+    unsigned int height;
+    unsigned int colorDepth;
+    unsigned int dataSize;
+
+    //find those in the header file
+    }
+
+void bmp8_saveImage(const char * filename, t_bmp8 * img){
+
+
+}
+
+void bmp8_free(t_bmp8 * img){
+
+
+}
+
+void bmp8_printInfo(t_bmp8 * img){
+
+
+}
--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/jalil/CLionProjects/Image_processing-Jalil-Bellahcen_Mael-Prouteau
+# Install script for directory: C:/Users/astri/CLionProjects/Image_processing_Jalil_Bellahcen_Mael_Prouteau
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -52,6 +52,6 @@ endif()
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-  file(WRITE "C:/Users/jalil/CLionProjects/Image_processing-Jalil-Bellahcen_Mael-Prouteau/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+  file(WRITE "C:/Users/astri/CLionProjects/Image_processing_Jalil_Bellahcen_Mael_Prouteau/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
