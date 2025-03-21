2c447ef22199dac05994d8518739ac0bbaf7ad69
Maël <78041675+Astrion20@users.noreply.github.com>
2025-03-21 12:22:05 +0100

    Hi

--- bmp8.c
+++ bmp8.c
@@ -6,22 +6,21 @@
 t_bmp8 * bmp8_loadImage(const char* filename){
     FILE *f = fopen(filename, "rb");
     unsigned char header[54];
-
+    fread(header, 1, 54, f);
+    unsigned int width = *(unsigned int *)&header[18];
+    unsigned int height = *(unsigned int *)&header[22];
+    unsigned int colorDepth = *(unsigned int *)&header[28];
+    unsigned int dataSize = *(unsigned int *)&header[34];
 
 
     t_bmp8 * new_image = (t_bmp8 *) malloc(sizeof(t_bmp8));
     if (new_image == NULL ) return NULL;
-    unsigned char *header = malloc(sizeof(unsigned char)*54);
+    *header = malloc(sizeof(unsigned char)*54);
     if (header == NULL)
         printf("Error with memory location");
 
     unsigned char* colorTable = (unsigned char*)malloc(sizeof(unsigned char)*1024);
     unsigned char* data = (unsigned char*)malloc(sizeof(unsigned char));
-
-    unsigned int width = *(unsigned int *)&header[18];
-    unsigned int height = *(unsigned int *)&header[22];
-    unsigned int colorDepth = *(unsigned int *)&header[28];
-    unsigned int dataSize = *(unsigned int *)&header[34];
 
     //find those in the header file
     }
