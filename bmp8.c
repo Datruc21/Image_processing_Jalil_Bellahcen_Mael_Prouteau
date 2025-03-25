0c95e508139aa81a587c84c8f3eb5667b80066af
Jalil Bellahcen <jalil.bellahcen@gmail.com>
2025-03-25 15:02:24 +0100

    test

--- bmp8.c
+++ bmp8.c
@@ -3,18 +3,27 @@
 #include <stdio.h>
 #include <string.h>
 
-t_bmp8 * bmp8_loadImage(const char* filename){
-    t_bmp8 * new_image = (t_bmp8 *) malloc(sizeof(t_bmp8));
-    if (new_image == NULL ) {printf("Problems in memory allocation");free(new_image);return NULL;}
+t_bmp8 * bmp8_loadImage(const char* filename) {
     FILE *f = fopen(filename, "rb");
+    if (f == NULL){printf("Error while reading the file!");return NULL;}
+    t_bmp8 * new_image = (t_bmp8 *) malloc(sizeof(t_bmp8));
+    if (new_image == NULL ) {printf("Problems in memory allocation");free(new_image);fclose(f);return NULL;}
     unsigned char header[54];
-    fread(header, 1, 54, f);
+    if (fread(header, 1, 54, f) != 54) {
+        printf("Error reading BMP header!\n");
+        free(new_image);
+        fclose(f);
+        return NULL;
+    }
+
     unsigned char colorTable[1024];
     unsigned int width = *(unsigned int *)&header[18];
     unsigned int height = *(unsigned int *)&header[22];
     unsigned int colorDepth = *(unsigned int *)&header[28];
     unsigned int dataSize = *(unsigned int *)&header[34];
-    strcpy(new_image -> header, header);
+    for (int i = 0; i<54;i++) {
+        *(new_image ->  header + i) = header[i];
+    }
     new_image -> width = width;
     new_image -> height = height;
     new_image -> colorDepth = colorDepth;
@@ -22,15 +31,20 @@ t_bmp8 * bmp8_loadImage(const char* filename){
     if (new_image -> colorDepth != 8) {
         printf("Image is not 8 bits deep!");
         free(new_image);
+        fclose(f);
         return NULL;
     }
-    new_image -> data = (unsigned char *)malloc(sizeof(unsigned char));
-    if (new_image -> data == NULL) {printf("Problems in memory allocation");free(new_image -> data);free(new_image);return NULL;}
+    new_image -> data = (unsigned char *)malloc(new_image -> dataSize);
+    if (new_image -> data == NULL) {printf("Problems in memory allocation");free(new_image -> data);free(new_image);fclose(f);return NULL;}
+    fread(new_image -> data,1,new_image -> dataSize,f);
+    fclose(f);
     return new_image;
-    // Ne pas oublier de fermer le fichier et de remplir data
 }
 
+
 void bmp8_saveImage(const char * filename, t_bmp8 * img){
+    FILE *f = fopen(filename, "wb");
+    if (f == NULL){printf("Error while opening the file!");return;}
 
 
 }
