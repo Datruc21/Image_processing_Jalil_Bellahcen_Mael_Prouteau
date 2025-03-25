ea6a3447d8f2a21d69ecc0ce0195b00c393509ed
Maël <78041675+Astrion20@users.noreply.github.com>
2025-03-25 14:16:58 +0100

    Hi

---
+++ cmake-build-debug/CMakeFiles/Image_processing_Jalil_Bellahcen_Mael_Prouteau.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Image_processing_Jalil_Bellahcen_Mael_Prouteau.dir/bmp8.c.obj"
+  "CMakeFiles/Image_processing_Jalil_Bellahcen_Mael_Prouteau.dir/bmp8.c.obj.d"
+  "CMakeFiles/Image_processing_Jalil_Bellahcen_Mael_Prouteau.dir/main.c.obj"
+  "CMakeFiles/Image_processing_Jalil_Bellahcen_Mael_Prouteau.dir/main.c.obj.d"
+  "Image_processing_Jalil_Bellahcen_Mael_Prouteau.exe"
+  "Image_processing_Jalil_Bellahcen_Mael_Prouteau.exe.manifest"
+  "Image_processing_Jalil_Bellahcen_Mael_Prouteau.pdb"
+  "libImage_processing_Jalil_Bellahcen_Mael_Prouteau.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Image_processing_Jalil_Bellahcen_Mael_Prouteau.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
