360811f5a5ce929173f857e83e16ef9e47ed3439
Jalil Bellahcen <jalil.bellahcen@gmail.com>
2025-03-28 12:03:39 +0100

    all functions finished (first part)

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 #include "bmp8.h"
 
 int main(void) {
-    t_bmp8 * new_image = bmp8_loadImage("lena_gray.bmp");
+        t_bmp8 * new_image = bmp8_loadImage("../lena_gray.bmp");
     bmp8_printInfo(new_image);
     bmp8_free(new_image);
     return 0;
